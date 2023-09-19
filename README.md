7150171361fe7d3ce3b80986c33e345ebea9b62b
dannnieel <daniel.osops@gmail.com>
2023-09-19 09:18:28 -0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# EvaluateNewsArticle2
+#  Udacity Project(NewArticle)
+
+Evaluation of Articles through Api Meaning Cloud.
+
+Don't forget to run the npm commands.
---
+++ src/client/__test__/handleSubmit.test.js
@@ -0,0 +1,9 @@
+
+
+const { handleSubmit } = require("../views/js/handleSubmit")
+
+describe('handleSubmit', ()=> {
+    it('returns something', () => {
+        expect(handleSubmit).toBeDefined();
+    })
+})
